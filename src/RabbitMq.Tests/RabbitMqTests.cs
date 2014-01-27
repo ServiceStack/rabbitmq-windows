cf703fea001e3bc19cc605de70e49f81b4cccd4a
Demis Bellot <demis.bellot@gmail.com>
2014-01-27 17:36:59 +0800

    Update test, add more screenshots

--- src/RabbitMq.Tests/RabbitMqTests.cs
+++ src/RabbitMq.Tests/RabbitMqTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 using NUnit.Framework;
 using RabbitMQ.Client;
 
@@ -12,16 +15,24 @@ namespace RabbitMq.Tests
         const string QueueName = "test.queue";
 
         [Test]
+        public void Register_durable_Exchange_and_Queue()
+        {            
+            using (IConnection conn = rabbitMqFactory.CreateConnection())
+            using (IModel channel = conn.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, "direct", durable: true, autoDelete: false, arguments: null);
+
+                channel.QueueDeclare(QueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueBind(QueueName, ExchangeName, routingKey: QueueName);
+            }
+        }
+
+        [Test]
         public void Publish_persistent_message_to_test_exchange()
         {
             using (IConnection conn = rabbitMqFactory.CreateConnection())
             using (IModel channel = conn.CreateModel())
             {
-                channel.ExchangeDeclare(ExchangeName, "direct", durable: true, autoDelete: false, arguments: null);
-                
-                channel.QueueDeclare(QueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-                channel.QueueBind(QueueName, ExchangeName, routingKey: QueueName);
-
                 var props = channel.CreateBasicProperties();
                 props.SetPersistent(true);
 
@@ -44,7 +55,7 @@ namespace RabbitMq.Tests
         }
 
         [Test]
-        public void Consume_message_from_Queue_Subscription()
+        public void Consume_one_message_from_Queue_Subscription()
         {
             using (IConnection conn = rabbitMqFactory.CreateConnection())
             using (IModel channel = conn.CreateModel())
@@ -56,6 +67,69 @@ namespace RabbitMq.Tests
 
                 var msgBody = Encoding.UTF8.GetString(msgResponse.Body);
                 Assert.That(msgBody, Is.EqualTo("Hello, World!"));
+            }
+        }
+
+        [Test]
+        public void Publish_5_messages_to_test_exchange()
+        {
+            using (IConnection conn = rabbitMqFactory.CreateConnection())
+            using (IModel channel = conn.CreateModel())
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    var props = channel.CreateBasicProperties();
+                    props.SetPersistent(true);
+
+                    var msgBody = Encoding.UTF8.GetBytes("Hello, World!");
+                    channel.BasicPublish(ExchangeName, routingKey: QueueName, basicProperties: props, body: msgBody);
+                }
+            }
+        }
+
+        [Test]
+        public void Consume_messages_from_Queue_Subscription()
+        {
+            using (IConnection conn = rabbitMqFactory.CreateConnection())
+            using (IModel channel = conn.CreateModel())
+            {
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume(QueueName, noAck: true, consumer: consumer);
+
+                ThreadPool.QueueUserWorkItem(_ => {
+                    var now = DateTime.UtcNow;
+                    while (DateTime.UtcNow - now < TimeSpan.FromSeconds(5))
+                    {
+                        var props = channel.CreateBasicProperties();
+                        props.SetPersistent(true);
+
+                        var msgBody = Encoding.UTF8.GetBytes("Hello, World!");
+                        channel.BasicPublish(ExchangeName, routingKey: QueueName, basicProperties: props, body: msgBody);
+
+                        Thread.Sleep(1000);
+                    }
+
+                    channel.Close();
+                });
+
+                while (true)
+                {
+                    try
+                    {
+                        var msgResponse = consumer.Queue.Dequeue(); //blocking
+
+                        var msgBody = Encoding.UTF8.GetString(msgResponse.Body);
+
+                        Console.WriteLine("Received Message: " + msgBody);
+
+                        Thread.Sleep(1000);
+                    }
+                    catch (EndOfStreamException ex)
+                    {
+                        Console.WriteLine("Channel was closed, Exiting...");
+                        break;
+                    }
+                }
             }
         }
 
